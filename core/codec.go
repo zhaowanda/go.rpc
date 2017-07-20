2bcfd56be9cfb02fa11978298f21a5b855d4bca3
wanda <1175061150@qq.com>
2017-07-20 17:49:38 +0800

    重写原生rpc框架中的clientCodec接口

--- core/codec.go
+++ core/codec.go
@@ -2,11 +2,22 @@ package core
 
 import context "golang.org/x/net/context"
 
+
 type ServerCodec interface {
 	ReadRequestHeader(context.Context, *Request) error
 	ReadRequestBody(context.Context, interface{}) error
 	// WriteResponse must be safe for concurrent use by multiple goroutines.
 	WriteResponse(context.Context, *Response, interface{}) error
+
+	Close() error
+}
+
+
+type ClientCodec interface {
+	// WriteRequest must be safe for concurrent use by multiple goroutines.
+	WriteRequest(context.Context, *Request, interface{}) error
+	ReadResponseHeader(context.Context, *Response) error
+	ReadResponseBody(context.Context, interface{}) error
 
 	Close() error
 }
