5c5346c41f3d3dbc1802ac5596869ba05b24fd35
wanda <1175061150@qq.com>
2017-07-21 14:36:02 +0800

    调整包结构

--- codec/protobuf_codec.go
+++ codec/protobuf_codec.go
@@ -4,14 +4,15 @@ import (
 	"io"
 	"github.com/zhaowanda/go.rpc/core"
 	"github.com/zhaowanda/go.rpc/codec/protobuf"
+	"github.com/zhaowanda/go.rpc/codec"
 )
 
 // NewProtobufServerCodec creates a protobuf ServerCodec by https://github.com/mars9/codec
-func NewProtobufServerCodec(conn io.ReadWriteCloser) core.ServerCodec {
+func NewProtobufServerCodec(conn io.ReadWriteCloser) codec.ServerCodec {
 	return protobuf.NewServerCodec(conn)
 }
 
 // NewProtobufClientCodec creates a protobuf ClientCodec by https://github.com/mars9/codec
-func NewProtobufClientCodec(conn io.ReadWriteCloser) core.ClientCodec {
+func NewProtobufClientCodec(conn io.ReadWriteCloser) codec.ClientCodec {
 	return protobuf.NewClientCodec(conn)
 }
