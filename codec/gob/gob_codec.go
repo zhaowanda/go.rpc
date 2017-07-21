24fac59109bcbc4c2d5ba7d478a6306721cc00c9
wanda <1175061150@qq.com>
2017-07-21 16:45:50 +0800

    解决循环依赖问题

--- codec/gob/gob_codec.go
+++ codec/gob/gob_codec.go
@@ -1,16 +1,17 @@
-package codec
+package gob
 
 import (
 	"io"
 	"github.com/zhaowanda/go.rpc/codec/gob"
+	"github.com/zhaowanda/go.rpc/codec"
 )
 
 // NewProtobufServerCodec creates a protobuf ServerCodec by https://github.com/mars9/codec
-func NewGobrCodec(conn io.ReadWriteCloser) ServerCodec {
+func NewGobrCodec(conn io.ReadWriteCloser) codec.ServerCodec {
 	return gob.NewServerCodec(conn)
 }
 
 // NewProtobufClientCodec creates a protobuf ClientCodec by https://github.com/mars9/codec
-func NewGobClientCodec(conn io.ReadWriteCloser) ClientCodec {
+func NewGobClientCodec(conn io.ReadWriteCloser) codec.ClientCodec {
 	return gob.NewClientCodec(conn)
 }
--- codec/protobuf/protobuf_codec.go
+++ codec/protobuf/protobuf_codec.go
@@ -1,4 +1,4 @@
-package codec
+package protobuf
 
 import (
 	"io"
