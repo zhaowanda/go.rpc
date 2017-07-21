81d6c16f65084b1c9b3054846ace5799f8cc5681
wanda <1175061150@qq.com>
2017-07-21 11:29:16 +0800

    构建rpc request 及 response

---
+++ core/rpcrequest.go
@@ -0,0 +1,10 @@
+package core
+
+// Request is a header written before every RPC call. It is used internally
+// but documented here as an aid to debugging, such as when analyzing
+// network traffic.
+type Request struct {
+	ServiceMethod string   // format: "Service.Method"
+	Seq           uint64   // sequence number chosen by client
+	next          *Request // for free list in Server
+}
